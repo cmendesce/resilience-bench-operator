474e940b2f05dbe6af7c549b1974929f4a6fc522
Carlos Mendes <cmendesce@gmail.com>
2025-10-07 19:28:36 -0300

    safe job deletion

--- resilience-bench/operator/src/main/java/io/resiliencebench/execution/DefaultScenarioExecutor.java
+++ resilience-bench/operator/src/main/java/io/resiliencebench/execution/DefaultScenarioExecutor.java
@@ -1,9 +1,12 @@
 package io.resiliencebench.execution;
 
 import io.fabric8.kubernetes.api.model.batch.v1.Job;
+import io.fabric8.kubernetes.api.model.batch.v1.JobList;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.Watcher;
 import io.fabric8.kubernetes.client.WatcherException;
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.ScalableResource;
 import io.resiliencebench.execution.steps.StepRegistry;
 import io.resiliencebench.execution.steps.k6.K6JobFactory;
 import io.resiliencebench.resources.queue.ExecutionQueue;
@@ -13,6 +16,8 @@ import io.resiliencebench.support.CustomResourceRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 import static io.resiliencebench.support.Annotations.*;
 import static java.util.Objects.nonNull;
@@ -53,15 +58,16 @@ public class DefaultScenarioExecutor implements ScenarioExecutor {
     executePreparationSteps(scenario, executionQueue);
     var executionQueueItem = executionQueue.getItem(scenario.getMetadata().getName());
     var job = k6JobFactory.create(scenario, workload, executionQueueItem);
-    
+
     // Add execution ID label for tracking
     if (executionQueue.getMetadata().getLabels().containsKey("execution-id")) {
       job.getMetadata().getLabels().put(EXECUTION_ID, executionQueue.getMetadata().getLabels().get("execution-id"));
     }
     
     var jobsClient = kubernetesClient.batch().v1().jobs();
-    jobsClient.inNamespace(job.getMetadata().getNamespace()).withName(job.getMetadata().getName()).delete();
+    deleteJob(jobsClient, job.getMetadata().getName(), ns);
     jobsClient.resource(job).create();
+
     logger.info("Job created: {}", job.getMetadata().getName());
     jobsClient.resource(job).watch(new Watcher<>() {
       @Override
@@ -86,6 +92,14 @@ public class DefaultScenarioExecutor implements ScenarioExecutor {
     });
   }
 
+  private static void deleteJob(MixedOperation<Job, JobList, ScalableResource<Job>> jobsClient, String jobName, String namespace) {
+    jobsClient.inNamespace(namespace).withName(jobName).delete();
+    jobsClient.inNamespace(namespace).withName(jobName).waitUntilCondition(
+            j -> j == null || nonNull(j.getStatus().getCompletionTime()),
+            2, TimeUnit.MINUTES
+    );
+    logger.info("Job deleted: {}", jobName);
+  }
 
   private void executePreparationSteps(Scenario scenario, ExecutionQueue queue) {
     stepRegistry.getPreparationSteps().forEach(step -> step.execute(scenario, queue));
